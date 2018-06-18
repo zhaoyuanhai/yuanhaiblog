709a92d78cf77810c7f71fb98a2a9bd2de06d329
yuan hai zhao <zhaoyuanhai66@outlook.com>
2018-06-18 10:22:18 +0800

    init

---
+++ YuanHai.BlogUI/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YuanHai.BlogUI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(route =>
+            {
+                //后台管理
+                route.MapRoute(
+                    name: "manager", 
+                    template: "mgr/{controller=Manager}/{action=Index}");
+
+                //页面
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
