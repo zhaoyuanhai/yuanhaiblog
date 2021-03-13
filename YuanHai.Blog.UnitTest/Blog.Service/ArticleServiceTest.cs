d32079fb69382369c4ce4cb8308f635b1e3e63b7
YUANHAI ZHAO <yuanhai.zhao@capgemini.com>
2021-03-13 10:47:04 +0800

    add unit test

---
+++ YuanHai.Blog.UnitTest/Blog.Service/ArticleServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Blog.IRepositories;
+using Blog.Repositories.Abstractions.Entities;
+using Blog.Services;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace YuanHai.Blog.UnitTest.Blog.Service
+{
+    public class ArticleServiceTest
+    {
+        [Fact]
+        public async Task MyTest()
+        {
+            var articleRespositoryMock = new Mock<IArticleRepository>();
+
+            var articleService = new ArticleService(articleRespositoryMock.Object);
+
+            await articleService.AddArticles(new ArticleEntities());
+
+            Assert.True(true);
+        }
+    }
+}
